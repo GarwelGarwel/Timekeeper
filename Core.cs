5301f443067f420d790f81df76ddc15e6d9f8895
GarwelGarwel <okozlovsky@gmail.com>
2018-08-22 16:22:08 +0300

    Added Core lib

---
+++ Core.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+
+namespace Timekeeper
+{
+    class Core
+    {
+        public static void ShowNotification(string msg) => ScreenMessages.PostScreenMessage(msg);
+
+        public static string GetString(ConfigNode n, string key, string defaultValue = null) => n.HasValue(key) ? n.GetValue(key) : defaultValue;
+
+        public static double GetDouble(ConfigNode n, string key, double defaultValue = 0)
+        {
+            double res;
+            try { res = Double.Parse(n.GetValue(key)); }
+            catch (Exception) { res = defaultValue; }
+            return res;
+        }
+
+        public static int GetInt(ConfigNode n, string key, int defaultValue = 0)
+        {
+            int res;
+            try { res = Int32.Parse(n.GetValue(key)); }
+            catch (Exception) { res = defaultValue; }
+            return res;
+        }
+
+        public static bool GetBool(ConfigNode n, string key, bool defaultValue = false)
+        {
+            bool res;
+            try { res = Boolean.Parse(n.GetValue(key)); }
+            catch (Exception) { res = defaultValue; }
+            return res;
+        }
+
+        /// <summary>
+        /// Log levels:
+        /// <list type="bullet">
+        /// <item><definition>None: do not log</definition></item>
+        /// <item><definition>Error: log only errors</definition></item>
+        /// <item><definition>Important: log only errors and important information</definition></item>
+        /// <item><definition>Debug: log all information</definition></item>
+        /// </list>
+        /// </summary>
+        public enum LogLevel { None, Error, Important, Debug };
+
+        /// <summary>
+        /// Current <see cref="LogLevel"/>: either Debug or Important
+        /// </summary>
+        public static LogLevel Level => LogLevel.Debug;
+
+        /// <summary>
+        /// Write into output_log.txt
+        /// </summary>
+        /// <param name="message">Text to log</param>
+        /// <param name="messageLevel"><see cref="LogLevel"/> of the entry</param>
+        public static void Log(string message, LogLevel messageLevel = LogLevel.Debug)
+        { if (messageLevel <= Level) Debug.Log("[Timekeeper] " + message); }
+    }
+}
