f4c99a12845c1fb2e21d3c8e1e53522b708fb2b9
GarwelGarwel <okozlovsky@gmail.com>
2018-08-27 17:43:41 +0300

    Added settings

---
+++ TimekeeperSettings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Timekeeper
+{
+    class TimekeeperSettings : GameParameters.CustomParameterNode
+    {
+        public override string Section => "Timekeeper";
+        public override string DisplaySection => Section;
+        public override string Title => "Timekeeper Settings";
+        public override int SectionOrder => 1;
+        public override GameParameters.GameMode GameMode => GameParameters.GameMode.ANY;
+        public override bool HasPresets => false;
+
+        [GameParameters.CustomParameterUI("Mod Enabled", toolTip = "Turn Timekeeper on/off")]
+        bool modEnabled = true;
+
+        public static bool ModEnabled
+        {
+            get => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().modEnabled;
+            set => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().modEnabled = value;
+        }
+
+        [GameParameters.CustomParameterUI("Count Orbits", toolTip = "Enable or disable sols counter")]
+        bool countOrbits = true;
+
+        public static bool CountOrbits
+        {
+            get => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().countOrbits;
+            set => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().countOrbits = value;
+        }
+
+        [GameParameters.CustomParameterUI("Count Sols", toolTip = "Enable or disable sols counter")]
+        bool countSols = true;
+
+        public static bool CountSols
+        {
+            get => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().countSols;
+            set => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().countSols = value;
+        }
+
+        [GameParameters.CustomParameterUI("Zero-based Counters", toolTip = "Start counting from orbit/sol 0 instead of 1")]
+        bool zeroCounters = false;
+
+        public static bool ZeroCounters
+        {
+            get => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().zeroCounters;
+            set => HighLogic.CurrentGame.Parameters.CustomParams<TimekeeperSettings>().zeroCounters = value;
+        }
+    }
+}
